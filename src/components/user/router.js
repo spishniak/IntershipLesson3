836d523aeae0dbeaeaf8390b0043833bdb8bc780
ivan <spishniak@gmail.com>
2021-12-02 22:20:44 +0200

    create all router

--- src/components/user/router.js
+++ src/components/user/router.js
@@ -5,7 +5,7 @@ const UserValidator = require('./validation');
 const userRouter = Router();
 userRouter.get('/', UserController.findAllUsers);
 userRouter.post('/', UserValidator.validateCreateUser, UserController.createdUser);
-userRouter.put('/:id', UserController.updateUser);
+userRouter.put('/:id', UserValidator.validateCreateUser, UserController.updateUser);
 userRouter.patch('/:id', UserController.updateUser);
 userRouter.delete('/:id', UserController.deleteUser);
 userRouter.get('/:id', UserController.findUserById);
--- src/config/router.js
+++ src/config/router.js
@@ -2,6 +2,13 @@ const userRouter = require('../components/user/router');
 
 function initt(server) {
   server.use('/v1/user', userRouter);
+
+  // eslint-disable-next-line no-unused-vars
+  server.use((err, req, res, nest) => {
+    res.status(404).json({
+      error: err?.message,
+    });
+  });
 }
 
 module.exports = {
